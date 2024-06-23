77c80ff77fba461ba32fc5bd7166894e2364d784
Alex <alex.dasilva@castgroup.com.br>
2024-06-23 00:41:53 -0300

    primeiro commit

---
+++ js/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulano de tal',
+                partido: 'ABC',
+                fotos:[
+                    {url:'38111.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos:[
+                    {url:'77222.jpg', legenda: 'Vereador', small: false}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos:[
+                    {url:'99.jpg', legenda: 'Prefeito'},
+                    {url:'99_9.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '88',
+                nome: 'Zulano',
+                partido: 'QWERE',
+                vice: 'Zul',
+                fotos:[
+                    {url:'88.jpg', legenda: 'Prefeito'},
+                    {url:'88_8.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
+    },
+]
